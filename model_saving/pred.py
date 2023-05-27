db8edcecb5454cfaa06da4e445830eca5a200309
Meghraj Konduskar <megh.konduskar@gmail.com>
2023-05-27 13:28:27 +0530

    Load model with Joblib

--- model_saving/pred.py
+++ model_saving/pred.py
@@ -1,7 +1,8 @@
 import pickle
+import joblib
 
 # load the model
-
-model = pickle.load(open("dib_79.pkl", "rb"))
+# model = pickle.load(open("dib_79.pkl", "rb"))
+model = joblib.load('dib_79_joblib.pkl')
 output = model.predict([[1, 1, 1, 1, 1, 1, 1, 1]])
 print(output)
